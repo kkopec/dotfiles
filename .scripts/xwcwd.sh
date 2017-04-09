d921b3c2f760f4bfd9ecfab7db4170834c4cdd40
kkopec <kopeckrzysztoff@gmail.com>
2017-04-09 19:23:13 +0200

    add script that finds active window cwd

---
+++ .scripts/xwcwd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+AWND=$(xprop -root _NET_ACTIVE_WINDOW | cut -d' ' -f5)
+APID=$(xprop -id $AWND _NET_WM_PID | cut -d' ' -f3)
+SPID=$(pgrep -P $APID zsh)
+SCWD=$(readlink -e /proc/$SPID/cwd)
+echo $SCWD
+

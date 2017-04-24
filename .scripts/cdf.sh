d92c3dd823d45f46a73d8146495eee7b69f3dbeb
kkopec <kopeckrzysztoff@gmail.com>
2017-04-24 23:09:33 +0200

    refactor cdf script
    it now allows traversing multiple directories and handle errors properly

--- .scripts/cdf.sh
+++ .scripts/cdf.sh
@@ -1,18 +1,48 @@
 #!/usr/bin/env bash
+#=====================================================================
+#
+# cdf - find and change directory
+#
+# DESCRIPTION
+#   Changes cwd to a directory located within another directories.
+#
+# USAGE
+#   cdf.sh DIR...
+#
+#     DIR     directory name (full of specific part)
+#
+#=====================================================================
 
 SCRIPT_NAME="${(%):-%N}"
 usage() {
-    echo "change directory - changes cwd to a directory located within another directory"
-    echo ""
-    echo "Usage: $(basename $SCRIPT_NAME) <fd> [fi]"
-    echo "Options:" 
-    echo "  <fd>    find top level directory"
-    echo "  [fi]    find inner directory"
-    echo ""
+    echo "cdf - find and change directory"
+    echo "Usage: $(basename $SCRIPT_NAME) DIR..."
 }
 
-[ $# -eq 0 ] && usage && return 1
-OUTER_DIR=$(find . -mindepth 1 -maxdepth 1 -type d -name "*$1*" -print -quit) || return 1
-INNER_DIR=$(find $OUTER_DIR -type d -name "*$2*" -print -quit) || return 1
-cd $INNER_DIR
+FDIR="."
+
+find_dir(){
+    FDIR=$(find $1 -mindepth 1 -maxdepth 1 -type d -name "*$2*" -print -quit)
+}
+
+find_inner(){
+    FDIR=$(find $1 -type d -name "*$2*" -print -quit)
+}
+
+if [ $# -eq 0 ]; then
+    usage >&2
+    return 1
+elif [ $# -eq 1 ]; then
+    find_dir $FDIR $1
+    [ ! $FDIR ] && return 2
+else
+    while [ $# -gt 1 ] ; do
+        find_dir $FDIR $1
+        [ ! $FDIR ] && return 2
+        shift
+    done
+    find_inner $FDIR $1
+    [ ! $FDIR ] && return 3
+fi
+cd $FDIR
 
